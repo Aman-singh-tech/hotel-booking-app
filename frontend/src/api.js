0e9ca3a92840e6b0ef8bc3e637976afc02ab4931
Aman <bablooaman.p2@gmail.com>
2026-06-30 21:35:27 +0530

    Update API base URL to include '/api' path

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "https://hotels-8oh7.onrender.com";
+const VITE_API_URL = "https://hotels-8oh7.onrender.com/api";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: VITE_API_URL,
   withCredentials: true,
 });
 
